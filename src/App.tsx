7053a4b4eae56d3d01f48c3b41fc5db0f73634fb
[SW5] 예은선 <ysvln0912@gmail.com>
2024-04-15 19:07:31 +0900

    chore : styled-component theme global 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { Outlet } from "react-router-dom";
+import GlobalStyles from "./styles/global";
+import theme from "./styles/theme";
+
 import BottomNavigationBar from "./components/common/bottomNavigationBar/BottomNavigationBar";
+import { ThemeProvider } from "styled-components";
 
 function App() {
   return (
     <>
-      <Outlet />
-      <BottomNavigationBar />
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Outlet />
+        <BottomNavigationBar />
+      </ThemeProvider>
     </>
   );
 }
