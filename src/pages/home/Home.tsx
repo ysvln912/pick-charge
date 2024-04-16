5d4aee746890ca2c7b8af734ed36f13fd241771e
[SW5] 예은선 <ysvln0912@gmail.com>
2024-04-16 16:10:25 +0900

    rename : Main 에서 Home 파일명 변경

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import * as S from "./Home.style";
+
+export default function Home() {
+  return <S.Test>Home, 메인페이지</S.Test>;
+}
--- src/pages/main/Main.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import * as S from "./Main.style";
-
-export default function Main() {
-  return <S.Test>Main, 메인페이지</S.Test>;
-}
