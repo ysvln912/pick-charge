cf302aaa4c1a16bca1c325ca38edefe2cbb7d14b
[SW5] 예은선 <ysvln0912@gmail.com>
2024-04-17 21:36:00 +0900

    feat : ErrorMessage 컴포넌트 작성

---
+++ src/components/common/errorMessage/ErrorMessage.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable react-refresh/only-export-components */
+import * as S from "./ErrorMessage.style";
+
+import { ReactNode } from "react";
+
+export interface ErrorMessageProps {
+  children: ReactNode;
+}
+
+export default function ErrorMessage(props: ErrorMessageProps) {
+  const { children } = props;
+  return (
+    <>
+      <S.ErrorMessage>{children}</S.ErrorMessage>
+    </>
+  );
+}
