ace311749393e855989709e8aabf9d9b047825c3
[AI6/2반] 박재희 <dalkey23@gmail.com>
2024-04-17 21:45:03 +0900

    fix : confirmDialog 버튼 수정 및 내용 속성 추가

--- src/components/common/confirmDialog/ConfirmDialog.style.tsx
+++ src/components/common/confirmDialog/ConfirmDialog.style.tsx
@@ -1,31 +1,40 @@
 import { styled } from "styled-components";
-import { ModalBackground } from "../../../styles/common";
+import {
+    ModalBackground,
+    flexSpaceBetweenCenter,
+} from "../../../styles/common";
 
 export const ConfirmContainer = styled.div`
     ${ModalBackground}
 `;
 export const Confirm = styled.div`
     width: 300px;
-    background-color :${({ theme }) => theme.PALETTE.white};
-    height: 13vh;
+    background-color: ${({ theme }) => theme.PALETTE.white};
     margin: 20rem auto;
-    padding: 2rem;
     border-radius: 1rem;
 `;
 
-export const ConfirmContents = styled.p`
+export const ConfirmTitle = styled.p`
     font-size: ${({ theme }) => theme.FONT_SIZE.md};
     font-weight: ${({ theme }) => theme.FONT_WEIGHT.bold};
-    text-align : center;
+    text-align: center;
+    padding: 1.2rem;
+`;
 
+export const ConfirmContents = styled.div`
+    width: 300px;
+    border-top: ${({ theme }) => theme.PALETTE.gray[200]} 1px solid;
+    padding: 1.2rem;
+    p {
+        font-size: ${({ theme }) => theme.FONT_SIZE.md};
+        font-weight: ${({ theme }) => theme.FONT_WEIGHT.medium};
+    }
 `;
 
 export const ButtonDiv = styled.div`
-    text-align : center;
-    padding : 1rem;
+    ${flexSpaceBetweenCenter}
+    padding: 0.5rem;
     * {
-        margin : 1rem;
-        background-color :${({ theme }) => theme.PALETTE.white};
+        margin: 0.3rem;
     }
-
-`
+`;
--- src/components/common/confirmDialog/ConfirmDialog.tsx
+++ src/components/common/confirmDialog/ConfirmDialog.tsx
@@ -1,22 +1,44 @@
 import React from "react";
 
 import * as S from "./ConfirmDialog.style";
+import Button from "../button/Button";
 
 interface ConfirmProps {
-    contents: string;
+    title: string;
+    contents?: string;
+    confirmButton: string;
+    cancelButton: string;
 }
 
+function InputDiv(props: { contents: string }) {
+    return (
+        <S.ConfirmContents>
+            <p>{props.contents}</p>
+            {/* input 수정 예정 */}
+            <input type="text" />
+        </S.ConfirmContents>
+    );
+}
 
-// 사용예시 : <ConfirmDialog  contents="로그아웃 할까요?"/>
-export default function ConfirmDialog(props:ConfirmProps) {
+// 예시1 : <ConfirmDialog title="로그아웃 할까요?" confirmButton="완료" cancelButton="취소"/>
+// 예시2 : <ConfirmDialog title="닉네임수정" contents="닉네임" confirmButton="완료" cancelButton="취소"/>
+export default function ConfirmDialog(props: ConfirmProps) {
     return (
         <S.ConfirmContainer>
             <S.Confirm>
-                <S.ConfirmContents>{props.contents}</S.ConfirmContents>
+                <S.ConfirmTitle>{props.title}</S.ConfirmTitle>
+                {props.contents ? (
+                    <InputDiv contents={props.contents} />
+                ) : (
+                    <></>
+                )}
                 <S.ButtonDiv>
-                    {/* 버튼 수정 예정 */}
-                    <button>확인</button>
-                    <button>취소</button>
+                    <Button size="big" category="nomal">
+                        {props.confirmButton}
+                    </Button>
+                    <Button size="big" category="retry">
+                        {props.cancelButton}
+                    </Button>
                 </S.ButtonDiv>
             </S.Confirm>
         </S.ConfirmContainer>
