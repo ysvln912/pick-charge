49ba7adf68ce5f7acd3c50e45bde51c1acf9754b
[AI6/2반] 박재희 <dalkey23@gmail.com>
2024-04-18 18:21:00 +0900

    style : ChargingInfo 스타일 수정

--- src/components/common/chargingInfo/ChargingInfo.style.tsx
+++ src/components/common/chargingInfo/ChargingInfo.style.tsx
@@ -1,32 +1,52 @@
 import styled from "styled-components";
 
-
-
-export const ChargingTitle = styled.div`
-    display: flex;
+export const ChargingContainer = styled.div`
+    border: ${({ theme }) => theme.PALETTE.gray[200]} 0.1rem solid;
+    padding: 0.7rem;
     margin: 0.8rem;
-    p {
+    border-radius: 1rem;
+`;
+export const ChargingContent = styled.div`
+    display : flex;
+    & .chargingTitle {
+        margin: 0.5rem;
         font-size: ${({ theme }) => theme.FONT_SIZE.md};
         font-weight: ${({ theme }) => theme.FONT_WEIGHT.bold};
     }
 `;
 
 export const ChargingAddress = styled.div`
-    margin: 0.8rem;
+    margin: 0.5rem;
     font-size: ${({ theme }) => theme.FONT_SIZE.sm};
     font-weight: ${({ theme }) => theme.FONT_WEIGHT.light};
     color: ${({ theme }) => theme.PALETTE.gray[300]};
 `;
 
 export const ChargingStatus = styled.div`
-    margin: 0.8rem;
+    margin: 0.5rem;
     display: flex;
-    color: #fdb022;
-    path {
-        fill: #fdb022;
-    }
+    align-items: center;
     .type {
         color: ${({ theme }) => theme.PALETTE.black};
     }
+    & p {
+        margin-left: 0.4rem;
+    }
+    ${({ theme }) => {
+        return `
+            &.restriction {
+                color: ${theme.PALETTE.gray[300]};
+                path {
+                    fill: ${theme.PALETTE.gray[300]};
+                }
+            }
+            &.available {
+                color: ${theme.PALETTE.yellow};
+                path {
+                    fill: ${theme.PALETTE.yellow};
+                }
+        `;
+    }}
+
 
 `;
--- src/components/common/chargingInfo/ChargingInfo.tsx
+++ src/components/common/chargingInfo/ChargingInfo.tsx
@@ -20,22 +20,26 @@ export interface Charger {
     charger_role: "개인" | "공공";
 }
 
-
 export default function ChargingInfo({ info }: { info: Charger }) {
     return (
-        <div>
-            <S.ChargingTitle>
-                <p>{info.charger_name}</p>
-                <ChargingRoleCard role={info.charger_role}/>
-                <div>별점</div>
-            </S.ChargingTitle>
+        <S.ChargingContainer>
+            <S.ChargingContent>
+                <p className="chargingTitle">{info.charger_name}</p>
+                <ChargingRoleCard role={info.charger_role} />
+            </S.ChargingContent>
             <S.ChargingAddress>{info.charger_location}</S.ChargingAddress>
-            <S.ChargingStatus>
-                {info.status === "이용가능" ? <BoltIcon /> : <BatterErrorIcon />}
+            <S.ChargingStatus
+                className={
+                    info.status === "이용가능" ? "available" : "restriction"
+                }>
+                {info.status === "이용가능" ? (
+                    <BoltIcon />
+                ) : (
+                    <BatterErrorIcon />
+                )}
                 <p>{info.status}</p>
                 <p className="type">{info.charger_type}</p>
             </S.ChargingStatus>
-            
-        </div>
+        </S.ChargingContainer>
     );
 }
