ff7d750b7b26eb04b44f439c2b8cf3f0ab1ea5be
[SW5] 예은선 <ysvln0912@gmail.com>
2024-04-19 01:31:04 +0900

    feat : InputBase 작성

---
+++ src/components/common/input/Base/InputBase.tsx
@@ -0,0 +1,58 @@
+/* eslint-disable react-refresh/only-export-components */
+import * as S from "./inputBase.style";
+import { HTMLAttributes, forwardRef, Ref, ReactNode, useState } from "react";
+
+type SizeType = "small" | "medium" | "full";
+type ShapeType = "default" | "round";
+type ColorType = "default" | "primary";
+
+export interface InputBaseProps extends HTMLAttributes<HTMLDivElement> {
+  error?: boolean;
+  size?: SizeType;
+  shape?: ShapeType;
+  color?: ColorType;
+  isFocus?: boolean;
+  disabled?: boolean;
+  children: ReactNode;
+}
+
+function InputBase(props: InputBaseProps, ref: Ref<HTMLInputElement>) {
+  const {
+    children,
+    error,
+    disabled,
+    size = "full",
+    shape = "default",
+    color = "default",
+  } = props;
+
+  const [isFocus, setIsFocus] = useState(false);
+  const handleFocus = () => {
+    setIsFocus(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocus(false);
+  };
+
+  return (
+    <>
+      <S.InputContainer
+        ref={ref}
+        size={size}
+        shape={shape}
+        color={color}
+        error={error}
+        disabled={disabled}
+        isFocus={isFocus}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        {...props}
+      >
+        {children}
+      </S.InputContainer>
+    </>
+  );
+}
+
+export default forwardRef(InputBase);
