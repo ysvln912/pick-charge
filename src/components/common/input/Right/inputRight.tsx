8cbc2cdce21bb6c8821c9bcd9d6cd4f8eecc825f
[SW5] 예은선 <ysvln0912@gmail.com>
2024-04-19 01:31:38 +0900

    feat : InputRight 작성

---
+++ src/components/common/input/Right/inputRight.tsx
@@ -0,0 +1,24 @@
+import * as S from "./inputRight.style";
+import { ReactNode } from "react";
+
+type ColorType = "default" | "primary";
+
+export interface InputRightProps {
+  children: ReactNode;
+  color: ColorType;
+  disabled?: boolean;
+}
+
+function InputRight({
+  color = "default",
+  disabled,
+  children,
+}: InputRightProps) {
+  return (
+    <S.Container color={color} disabled={disabled}>
+      {children}
+    </S.Container>
+  );
+}
+
+export default InputRight;
