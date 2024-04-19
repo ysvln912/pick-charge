350b767d5ccc0a044e1d7346950c59726d6d6131
JinKyu <rlawlsrb167@naver.com>
2024-04-19 20:53:10 +0900

    feat: 공통 컴포넌트 이미지 슬라이더 추가

---
+++ src/components/common/photoSlider/PhotoSlider.tsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import * as S from "./PhotoSlider.style";
+import { AnimatePresence, wrap } from "framer-motion";
+import RightIcon from "../icons/RightIcon";
+import LeftIcon from "../icons/LeftIcon";
+
+interface PhotoSliderProps {
+  imgs: string[];
+}
+
+export default function PhotoSlider({ imgs }: PhotoSliderProps) {
+  const [page, setPage] = useState(0);
+  const [direction, setDirection] = useState(0);
+
+  const imgIndex = wrap(0, imgs.length, page);
+
+  const paginate = (newDirection: number) => {
+    setPage(page + newDirection);
+    setDirection(newDirection);
+  };
+
+  return (
+    <S.Container>
+      <S.PrevBtn onClick={() => paginate(-1)}>
+        <LeftIcon />
+      </S.PrevBtn>
+      <S.ImgBox>
+        <AnimatePresence initial={false} custom={direction}>
+          <S.Img
+            key={page}
+            src={imgs[imgIndex]}
+            custom={direction}
+            variants={imgVariants}
+            initial="enter"
+            animate="center"
+            exit="exit"
+            transition={{
+              x: { type: "tween" },
+              opacity: { duration: 0.2 },
+            }}
+            drag="x"
+            dragConstraints={{ left: 0, right: 0 }}
+            dragElastic={1}
+            onDragEnd={(e, { offset, velocity }) => {
+              const swipe = swipePower(offset.x, velocity.x);
+              if (swipe < -swipeConfidenceThreshold) {
+                paginate(1);
+              } else if (swipe > swipeConfidenceThreshold) {
+                paginate(-1);
+              }
+            }}
+          />
+        </AnimatePresence>
+      </S.ImgBox>
+      <S.NextBtn onClick={() => paginate(1)}>
+        <RightIcon />
+      </S.NextBtn>
+    </S.Container>
+  );
+}
+
+const imgVariants = {
+  enter: (direction: number) => {
+    return {
+      x: direction > 0 ? 342 : -342,
+      opacity: 0,
+    };
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+  },
+  exit: (direction: number) => {
+    return {
+      zIndex: 0,
+      x: direction < 0 ? 342 : -342,
+      opacity: 0,
+    };
+  },
+};
+
+const swipeConfidenceThreshold = 10000;
+const swipePower = (offset: number, velocity: number) => {
+  return Math.abs(offset) * velocity;
+};
