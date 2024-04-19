4c65ceda245ef406d4afed9ba31a8e87b674c964
[SW5] 예은선 <ysvln0912@gmail.com>
2024-04-20 00:09:59 +0900

    comment : toast 사용 예시 작성

--- src/components/common/toast/Toast.tsx
+++ src/components/common/toast/Toast.tsx
@@ -14,3 +14,25 @@ export function Toast({ type = "default", message }: ToastProps) {
     </S.Container>
   );
 }
+
+/**
+ * @example
+ * import { useToast } from "@/hooks/useToast";
+ * 
+ * export default function Test() {
+ * const { triggerToast } = useToast();
+
+  const showToast = () => {
+    // 기본 값 default
+    // triggerToast("토스트 보여주기!");
+    // 성공 success
+    // triggerToast("토스트 보여주기!", "success");
+    // 에러 error
+    triggerToast("토스트 보여주기!", "error");
+  };
+
+  <Button size="lg" category="nomal" onClick={showToast}>
+        클릭하면 토스트가 보여요
+  </Button>
+ * }
+ * */
