19acc357f779541fe6c4475aede02b8a0c71c6dd
[AI6/2반] 박재희 <dalkey23@gmail.com>
2024-04-20 01:54:04 +0900

    fix : ConfirmDialog props 수정

--- src/components/common/confirmDialog/ConfirmDialog.style.tsx
+++ src/components/common/confirmDialog/ConfirmDialog.style.tsx
@@ -12,28 +12,30 @@ export const Confirm = styled.div`
     background-color: ${({ theme }) => theme.PALETTE.white};
     margin: 20rem auto;
     border-radius: 1rem;
-`;
-
-export const ConfirmTitle = styled.p`
-    font-size: ${({ theme }) => theme.FONT_SIZE.md};
-    font-weight: ${({ theme }) => theme.FONT_WEIGHT.bold};
-    text-align: center;
-    padding: 1.2rem;
+    padding-top: 1.2rem;
+    & .title {
+        font-size: ${({ theme }) => theme.FONT_SIZE.md};
+        font-weight: ${({ theme }) => theme.FONT_WEIGHT.bold};
+        text-align: center;
+    }
+    & .dialog {
+        padding-bottom : 1rem;
+        border-bottom: ${({ theme }) => theme.PALETTE.gray[200]} 1px solid;
+    }
+    & .confirm {
+        padding: 1.2rem;
+    }
 `;
 
 export const ConfirmContents = styled.div`
-    width: 300px;
-    border-top: ${({ theme }) => theme.PALETTE.gray[200]} 1px solid;
-    padding: 1.2rem;
-    p {
-        font-size: ${({ theme }) => theme.FONT_SIZE.md};
-        font-weight: ${({ theme }) => theme.FONT_WEIGHT.medium};
-    }
+    border: ${({ theme }) => theme.PALETTE.black} 1rem soild;
+    padding: 1.5rem;
 `;
 
 export const ButtonDiv = styled.div`
     ${flexSpaceBetweenCenter}
     padding: 0.5rem;
+
     * {
         margin: 0.3rem;
     }
