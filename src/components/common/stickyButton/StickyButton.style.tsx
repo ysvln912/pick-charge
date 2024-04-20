934eb64904ead451805e9e4ab2733e909a39e2a6
[SW5] 예은선 <ysvln0912@gmail.com>
2024-04-20 12:41:09 +0900

    feat : StickyButton 컴포넌트 작성

---
+++ src/components/common/stickyButton/StickyButton.style.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+interface Props {
+  disabled: boolean;
+}
+
+export const Container = styled.div<Props>`
+  position: fixed;
+  // 하단 네비게이션 높이값
+  bottom: 48.8px;
+  width: 390px;
+  cursor: ${({ disabled }) => (disabled ? "default" : "pointer")};
+  padding: 1rem;
+  background-color: ${({ theme, disabled }) =>
+    disabled ? theme.PALETTE.gray[100] : theme.PALETTE.mainColor};
+`;
+
+export const BtnText = styled.p<Props>`
+  text-align: center;
+  color: ${({ theme, disabled }) =>
+    disabled ? theme.PALETTE.gray[300] : theme.PALETTE.white};
+  font-weight: ${({ theme }) => theme.FONT_WEIGHT.bold};
+  font-size: ${({ theme }) => theme.FONT_SIZE.sm};
+`;
