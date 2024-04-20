d9129aae84a91f1b441af80d5d351932a0d6f1e8
[SW5] 예은선 <ysvln0912@gmail.com>
2024-04-20 11:23:55 +0900

    style : 회원가입 페이지 퍼블리싱

--- src/pages/signUp/SignUp.tsx
+++ src/pages/signUp/SignUp.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import * as S from "./SignUp.style";
+
+import { Link } from "react-router-dom";
+import AccountForm from "@/components/pages/signup/form/account/AccountForm";
+import TopNavigationBar from "@/components/common/topNavigationBar/TopNavigationBar";
+import ArrowLeftIconBtn from "@/components/common/iconButtons/ArrowLeftIconBtn";
+// import UserInfoForm from "@/components/pages/signup/form/userInfo/UserInfoForm";
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <>
+      <TopNavigationBar leftBtn={<ArrowLeftIconBtn />} text="회원가입" />
+      <S.Container>
+        <AccountForm />
+        {/* <UserInfoForm /> */}
+        <S.TextWrapper>
+          <p>
+            이미 가입하셨나요?
+            <Link to="/login">
+              <span>로그인</span>
+            </Link>
+          </p>
+        </S.TextWrapper>
+      </S.Container>
+    </>
+  );
 }
