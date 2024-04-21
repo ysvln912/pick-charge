2489636e437ea6f60a287674445f53d59a1b4d19
[AI6/2반] 박재희 <dalkey23@gmail.com>
2024-04-22 03:11:02 +0900

    fix : ConfirmDialog 기본 타입 confirm으로 수정

--- src/components/common/confirmDialog/ConfirmDialog.style.tsx
+++ src/components/common/confirmDialog/ConfirmDialog.style.tsx
@@ -1,8 +1,18 @@
-import { styled } from "styled-components";
+import styled, { css } from "styled-components";
 import {
     ModalBackground,
     flexSpaceBetweenCenter,
 } from "../../../styles/common";
+
+const confirmType = {
+    dialog: css`
+        padding-bottom: 1rem;
+        border-bottom: ${({ theme }) => theme.PALETTE.gray[200]} 1px solid;
+    `,
+    confirm: css`
+        padding: 1.2rem;
+    `,
+};
 
 export const ConfirmContainer = styled.div`
     ${ModalBackground}
@@ -13,18 +23,13 @@ export const Confirm = styled.div`
     margin: 20rem auto;
     border-radius: 1rem;
     padding-top: 1.2rem;
-    & .title {
-        font-size: ${({ theme }) => theme.FONT_SIZE.md};
-        font-weight: ${({ theme }) => theme.FONT_WEIGHT.bold};
-        text-align: center;
-    }
-    & .dialog {
-        padding-bottom : 1rem;
-        border-bottom: ${({ theme }) => theme.PALETTE.gray[200]} 1px solid;
-    }
-    & .confirm {
-        padding: 1.2rem;
-    }
+`;
+
+export const ConfirmTitle = styled.div<{ type: "confirm" | "dialog" }>`
+    font-size: ${({ theme }) => theme.FONT_SIZE.md};
+    font-weight: ${({ theme }) => theme.FONT_WEIGHT.bold};
+    text-align: center;
+    ${({ type }) => confirmType[type]}
 `;
 
 export const ConfirmContents = styled.div`
