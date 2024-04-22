9bcd97f11d3f0cdd0917884d0cc3354603330a13
JinKyu <rlawlsrb167@naver.com>
2024-04-23 00:29:47 +0900

    feat: 충전기 등록페이지 RadioButton 컴포넌트 추가

---
+++ src/components/pages/registerCharger/RadioButton.tsx
@@ -0,0 +1,66 @@
+import { flexAlignCenter } from "@/styles/common";
+import React, { ChangeEvent } from "react";
+import styled from "styled-components";
+
+export interface RadioButtonProps {
+  id: "fast" | "slow";
+  value: "급속" | "완속";
+  selectedOption: string | null;
+  handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function RadioButton({
+  id,
+  value,
+  selectedOption,
+  handleChange,
+}: RadioButtonProps) {
+  return (
+    <Container>
+      <RadioShape htmlFor={id} checked={selectedOption === value}></RadioShape>
+      <Input
+        type="radio"
+        name="speed"
+        id={id}
+        value={value}
+        onChange={handleChange}
+      />
+      <Label htmlFor={id}>{value}</Label>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  ${flexAlignCenter};
+`;
+
+const RadioShape = styled.label<{ checked: boolean }>`
+  width: 18px;
+  height: 18px;
+  display: inline-block;
+  border-radius: 50%;
+  border: 1px solid ${({ theme }) => theme.PALETTE.mainColor};
+  position: relative;
+  cursor: pointer;
+  &::before {
+    display: ${({ checked }) => (checked ? "block" : "none")};
+    content: "";
+    width: 10px;
+    height: 10px;
+    background-color: ${({ theme }) => theme.PALETTE.mainColor};
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 50%;
+  }
+`;
+
+const Input = styled.input`
+  display: none;
+`;
+
+const Label = styled.label`
+  font-size: ${({ theme }) => theme.FONT_SIZE.sm};
+  margin: 0 4px;
+`;
--- src/pages/registerCharger/RegisterCharger.tsx
+++ src/pages/registerCharger/RegisterCharger.tsx
@@ -1,5 +1,69 @@
-import React from "react";
+import IconButton from "@/components/common/iconButton/IconButton";
+import Label from "@/components/common/label/Label";
+import SearchInput from "@/components/common/searchInput/SearchInput";
+import TopNavigationBar from "@/components/common/topNavigationBar/TopNavigationBar";
+import RadioButton from "@/components/pages/registerCharger/RadioButton";
+import { flexAlignCenter, flexColumn } from "@/styles/common";
+import React, { ChangeEvent, useState } from "react";
+import styled from "styled-components";
 
 export default function RegisterCharger() {
-  return <div>RegisterCharger</div>;
+  const [error, setError] = useState(false);
+  const [address, setAddress] = useState("");
+  const [speed, setSpeed] = useState<string | null>(null);
+  console.log(speed);
+  const handleSpeedChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.currentTarget;
+    setSpeed(value);
+  };
+  return (
+    <Container>
+      <TopNavigationBar
+        leftBtn={<IconButton icon="arrowLeft" />}
+        text="충전기 등록"
+      />
+      <SearchInput
+        label="충전소"
+        placeholder="충전소 주소를 입력해 주세요."
+        error={error}
+        errorMessage="필수 입력 항목입니다."
+        name="address"
+        value={address}
+      />
+      <Label size="lg">충전기 정보</Label>
+      <ChargingSpeed>
+        <Label size="md">충전 속도</Label>
+        <SpeedInputTag>
+          <RadioButton
+            id="fast"
+            value="급속"
+            selectedOption={speed}
+            handleChange={handleSpeedChange}
+          />
+          <RadioButton
+            id="slow"
+            value="완속"
+            selectedOption={speed}
+            handleChange={handleSpeedChange}
+          />
+        </SpeedInputTag>
+      </ChargingSpeed>
+    </Container>
+  );
 }
+
+const Container = styled.section`
+  padding: 24px;
+  padding-top: 80px;
+  ${flexColumn};
+`;
+
+const ChargingSpeed = styled.div`
+  ${flexColumn};
+  padding: 8px 0;
+`;
+
+const SpeedInputTag = styled.div`
+  ${flexAlignCenter};
+  padding: 8px 0;
+`;
