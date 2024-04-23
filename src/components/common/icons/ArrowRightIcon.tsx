cc86a84f995fa1796e91e122f800fa00b938dd22
[SW5] 예은선 <ysvln0912@gmail.com>
2024-04-23 18:50:12 +0900

    add : 편집 아이콘 추가

---
+++ src/components/common/icons/ArrowRightIcon.tsx
@@ -0,0 +1,16 @@
+export default function ArrowRightIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="4"
+      height="7"
+      viewBox="0 0 4 7"
+      fill="none"
+    >
+      <path
+        d="M2.48649 3.5L0 0.816667L0.756757 0L4 3.5L0.756757 7L0 6.18333L2.48649 3.5Z"
+        fill="#888888"
+      />
+    </svg>
+  );
+}
