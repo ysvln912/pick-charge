b0d4becfbe97359c6c25da5a3c8ca55ea7722840
[AI6/2반] 박재희 <dalkey23@gmail.com>
2024-04-23 15:53:28 +0900

    feat : 카카오 api 이용해서 지도 표현

--- src/pages/chargingMap/ChargingMap.tsx
+++ src/pages/chargingMap/ChargingMap.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+declare global {
+    interface Window {
+        kakao: any;
+    }
+}
 
 export default function ChargingMap() {
-  return <div>ChargingMap</div>;
+    useEffect(() => {
+        let container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
+        let options = {
+            //지도를 생성할 때 필요한 기본 옵션
+            center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+            level: 3, //지도의 레벨(확대, 축소 정도)
+        };
+
+        let map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    }, []);
+    return (
+        <div>
+            <p>ChargingMap</p>
+            <div id="map" style={{ width: "390px", height: "100vh" }} />
+        </div>
+    );
 }
+
