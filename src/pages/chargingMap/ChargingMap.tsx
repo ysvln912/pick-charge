7e24a22b2ed81ebd5d3712ac4cdaea183c1ae3cd
[AI6/2반] 박재희 <dalkey23@gmail.com>
2024-04-23 16:22:14 +0900

    fix : ChargerMap 컴포넌트 분리

--- src/pages/chargingMap/ChargingMap.tsx
+++ src/pages/chargingMap/ChargingMap.tsx
@@ -1,10 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Charger } from "@/components/common/chargingInfo/ChargingInfo";
-declare global {
-    interface Window {
-        kakao: any;
-    }
-}
+import ChargerMap from "@/components/pages/charger/chargerMap/ChargerMap";
 
 export default function ChargingMap() {
     const sampleData: Charger[] = [
@@ -78,57 +74,11 @@ export default function ChargingMap() {
         },
     ];
 
-    useEffect(() => {
-        let container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
-        let options = {
-            //지도를 생성할 때 필요한 기본 옵션
-            center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-            level: 5, //지도의 레벨(확대, 축소 정도)
-        };
-
-        let map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-
-        if (navigator.geolocation) {
-            // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-            navigator.geolocation.getCurrentPosition(function (position) {
-                var lat = position.coords.latitude, // 위도
-                    lon = position.coords.longitude; // 경도
-
-                var locPosition = new window.kakao.maps.LatLng(lat, lon);
-                // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-
-                map.setCenter(locPosition);
-            });
-        } else {
-            // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-            console.log("geolocation을 사용할수 없어요..");
-        }
-
-        // 마커 이미지의 이미지 주소입니다
-        var imageSrc =
-            "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
-
-        for (var i = 0; i < sampleData.length; i++) {
-            // 마커 이미지의 이미지 크기 입니다
-            var imageSize = new window.kakao.maps.Size(24, 35);
-
-            // 마커 이미지를 생성합니다
-            var markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize);
-
-            // 마커를 생성합니다
-            var marker = new window.kakao.maps.Marker({
-                map: map, // 마커를 표시할 지도
-                position: new window.kakao.maps.LatLng(sampleData[i].latitude, sampleData[i].longitude), // 마커를 표시할 위치
-                title: sampleData[i].charger_name, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-                image: markerImage, // 마커 이미지
-            });
-        }
-
-    }, []);
+    
     return (
         <div>
             <p>ChargingMap</p>
-            <div id="map" style={{ width: "390px", height: "100vh" }} />
+            <ChargerMap info ={sampleData}/>
         </div>
     );
 }
