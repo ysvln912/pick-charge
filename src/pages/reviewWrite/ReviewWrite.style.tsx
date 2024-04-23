7a3f61f7e63901eefed21620bc952f91628f9761
[SW5] 예은선 <ysvln0912@gmail.com>
2024-04-23 18:48:15 +0900

    style : 리뷰 작성 페이지 퍼블리싱

---
+++ src/pages/reviewWrite/ReviewWrite.style.tsx
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+import { flexColumn } from "@/styles/common";
+
+export const Container = styled.div`
+  padding: 72px 1.5rem;
+  ${flexColumn}
+  justify-content: center;
+  gap: 1rem;
+`;
+
+export const Box = styled.div`
+  ${flexColumn}
+`;
