ce201e4b131cdf6891dd31d28ed6a54a036c7b2a
[AI6/2반] 박재희 <dalkey23@gmail.com>
2024-04-23 16:08:09 +0900

    style : BottomNavigationBar 배경색, 높이 수정

--- src/components/common/bottomNavigationBar/BottomNavigationBar.style.tsx
+++ src/components/common/bottomNavigationBar/BottomNavigationBar.style.tsx
@@ -4,8 +4,10 @@ import { bodyContainer, flexSpaceBetweenCenter } from "../../../styles/common";
 export const BottomContainer = styled.div`
   position: fixed;
   bottom: 0;
-  height: 2.5rem;
+  height: 68px;
   padding: 1.5rem;
+  z-index: 999;
+  background-color : ${({ theme }) => theme.PALETTE.white};
   font-size: ${({ theme }) => theme.FONT_SIZE.es};
   font-weight: ${({ theme }) => theme.FONT_WEIGHT.light};
   border-top: ${({ theme }) => theme.PALETTE.gray[100]} 1px solid;
