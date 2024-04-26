780d8b75ad9a70ae85d3456cf5de89846a6d4ae4
[SW5] 예은선 <ysvln0912@gmail.com>
2024-04-26 22:04:14 +0900

    feat : 회원가입 페이지

--- src/pages/signUp/SignUp.tsx
+++ src/pages/signUp/SignUp.tsx
@@ -1,31 +1,52 @@
 import * as S from "./SignUp.style";
 
-import { Link } from "react-router-dom";
+import { useState } from "react";
+
 import IconButton from "@/components/common/iconButton/IconButton";
 import AccountForm from "@/components/pages/signup/form/account/AccountForm";
 import TopNavigationBar from "@/components/common/topNavigationBar/TopNavigationBar";
-import SearchInput from "@/components/common/searchInput/SearchInput";
-// import UserInfoForm from "@/components/pages/signup/form/userInfo/UserInfoForm";
+import UserInfoForm from "@/components/pages/signup/form/userInfo/UserInfoForm";
+import Done from "@/components/pages/signup/done/Done";
+import { UserType } from "@/types";
+import { useFunnel } from "@/hooks/useFunnel";
+
+const signupSteps = ["계정 정보", "유저 정보", "가입 완료"];
 
 export default function SignUp() {
+  const { Funnel, Step, currentStep, handleClickNext } = useFunnel(
+    signupSteps[0]
+  );
+
+  const [data, setData] = useState<UserType>({
+    email: "",
+    password: "",
+    name: "",
+    nickname: "",
+  });
+
   return (
     <>
-      <TopNavigationBar
-        text="회원가입"
-        leftBtn={<IconButton icon={"arrowLeft"} />}
-      />
+      {currentStep !== signupSteps[signupSteps.length - 1] && (
+        <TopNavigationBar
+          text="회원가입"
+          leftBtn={<IconButton icon={"arrowLeft"} />}
+        />
+      )}
       <S.Container>
-        <SearchInput label="주유소" />
-        <AccountForm />
-        {/* <UserInfoForm /> */}
-        <S.TextWrapper>
-          <p>
-            이미 가입하셨나요?
-            <Link to="/login">
-              <span>로그인</span>
-            </Link>
-          </p>
-        </S.TextWrapper>
+        <Funnel>
+          <Step name={signupSteps[0]}>
+            <AccountForm
+              data={data}
+              onNext={() => handleClickNext(signupSteps[1])}
+            />
+          </Step>
+          <Step name={signupSteps[1]}>
+            <UserInfoForm onNext={() => handleClickNext(signupSteps[2])} />
+          </Step>
+          <Step name={signupSteps[2]}>
+            <Done />
+          </Step>
+        </Funnel>
       </S.Container>
     </>
   );
