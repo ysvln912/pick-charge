b3b511e9ad54ae1e53b967c249a3d0477b8f53b5
[SW5] 예은선 <ysvln0912@gmail.com>
2024-04-26 22:06:33 +0900

    feat : 로그인 페이지

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,12 +1,34 @@
 import * as S from "./Login.style";
 
 import { Link } from "react-router-dom";
+
 import logo from "@/assets/imgs/logo_big.png";
 import Button from "@/components/common/button/Button";
 import LabelInput from "@/components/common/labelInput/LabelInput";
 import TopNavigationBar from "@/components/common/topNavigationBar/TopNavigationBar";
 
+import { useFormValidation } from "@/hooks/useFormValidation";
+import { useLogin } from "@/hooks/queries/user";
+
 export default function Login() {
+  const initialState = {
+    email: "",
+    password: "",
+  };
+
+  const { formState, handleInputChange, error, handleSubmit } =
+    useFormValidation(initialState);
+
+  // const { login } = useLogin();
+
+  const isFormValid =
+    !Object.keys(error).length && formState.email && formState.password;
+
+  const test = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    console.log("로그인 성공!");
+  };
+
   return (
     <>
       <TopNavigationBar text="로그인" />
@@ -15,14 +37,27 @@ export default function Login() {
           <img src={logo} alt="피카충전 로고" />
         </S.LogoWrapper>
 
-        <S.Form>
-          <LabelInput label="이메일" placeholder="example@picka.site" />
+        {/* <S.Form onSubmit={handleSubmit(() => login(formState))}> */}
+        <S.Form onSubmit={test}>
           <LabelInput
+            name="email"
+            label="이메일"
+            placeholder="example@picka.site"
+            value={formState.email}
+            onChange={handleInputChange("email")}
+            error={error.email}
+          />
+          <LabelInput
+            name="password"
+            type="password"
             label="비밀번호"
             placeholder="비밀번호를 입력해 주세요."
+            value={formState.password}
+            onChange={handleInputChange("password")}
+            error={error.password}
           />
           <S.ButtonWrapper>
-            <Button size="lg" category="normal">
+            <Button size="lg" category={isFormValid ? "normal" : "disable"}>
               이메일 로그인
             </Button>
           </S.ButtonWrapper>
