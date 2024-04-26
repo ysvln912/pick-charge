6b4a2e7bdd73d2e60d0bf193a4ad0cdf9d59c99a
[SW5] 예은선 <ysvln0912@gmail.com>
2024-04-26 21:48:24 +0900

    feat : 회원가입 완료 페이지 로그인 하러 가기 버튼 이동

--- src/components/pages/signup/done/Done.tsx
+++ src/components/pages/signup/done/Done.tsx
@@ -1,8 +1,16 @@
-import Button from "@/components/common/button/Button";
 import * as S from "./Done.style";
+
+import { useNavigate } from "react-router-dom";
+
+import Button from "@/components/common/button/Button";
 import done from "@/assets/imgs/done.png";
 
 export default function Done() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/login");
+  };
   return (
     <S.Container>
       <S.ImageWrapper>
@@ -13,7 +21,7 @@ export default function Done() {
         <p>회원가입이 완료 되었어요.</p>
         <p>로그인 후 피카충전을 이용해 보세요!</p>
       </S.Content>
-      <Button category="normal" size="lg">
+      <Button category="normal" size="lg" onClick={handleClick}>
         로그인 하러 가기
       </Button>
     </S.Container>
