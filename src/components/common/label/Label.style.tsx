44921bdb1113fd4beaa6d2e5d8379a8b689f822f
[SW5] 예은선 <ysvln0912@gmail.com>
2024-04-27 21:42:23 +0900

    fix : Label 컴포넌트 require 수정

--- src/components/common/label/Label.style.tsx
+++ src/components/common/label/Label.style.tsx
@@ -1,4 +1,5 @@
 import styled, { css } from "styled-components";
+import { flexAlignCenter } from "@/styles/common";
 import { LabelProps } from "./Label";
 import { ObjMap } from "@/types";
 
@@ -15,5 +16,15 @@ export const Label = styled.label<LabelProps>`
   ${({ size }) => size && sizeCSS[size]}
   display: inline-block;
   font-weight: ${({ theme }) => theme.FONT_WEIGHT.medium};
+`;
+
+export const Container = styled.div`
+  ${flexAlignCenter}
+  gap: 4px;
   margin-bottom: 0.5rem;
+`;
+
+export const Require = styled.p`
+  color: ${({ theme }) => theme.PALETTE.mainColor};
+  font-size: ${({ theme }) => theme.FONT_SIZE.sm};
 `;
