766727cc076f67079ee58b2978cdbf11faa2dec1
[SW5] 예은선 <ysvln0912@gmail.com>
2024-04-27 21:39:25 +0900

    feat : 리뷰 작성 페이지

--- src/pages/reviewWrite/ReviewWrite.style.tsx
+++ src/pages/reviewWrite/ReviewWrite.style.tsx
@@ -2,7 +2,7 @@ import { styled } from "styled-components";
 import { flexColumn } from "@/styles/common";
 
 export const Container = styled.div`
-  padding: 72px 1.5rem;
+  padding: 72px 1.5rem 12.5rem;
   ${flexColumn}
   justify-content: center;
   gap: 1rem;
