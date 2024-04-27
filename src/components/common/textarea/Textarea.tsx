ebd1aa3923006e9754c5ead515e05cbfdde80774
[SW5] 예은선 <ysvln0912@gmail.com>
2024-04-27 21:47:02 +0900

    fix : textarea 컴포넌트 require 수정

--- src/components/common/textarea/Textarea.tsx
+++ src/components/common/textarea/Textarea.tsx
@@ -9,19 +9,33 @@ export interface TextareaProps
   extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   size?: SizeType;
   label?: ReactNode;
-  error?: boolean;
+  error?: boolean | string;
   errorMessage?: string;
+  name?: string;
+  require?: boolean;
 }
 
 function Textarea(
-  { label, size = "md", error, errorMessage, name, ...rest }: TextareaProps,
+  {
+    label,
+    size = "md",
+    error = false,
+    errorMessage,
+    name,
+    require = false,
+    ...rest
+  }: TextareaProps,
   ref: Ref<HTMLTextAreaElement>
 ) {
   return (
     <div>
-      {label && <Label htmlFor={name}>{label}</Label>}
-      <S.Textarea error={error} ref={ref} size={size} {...rest} />
-      <ErrorMessage visible={error}>{errorMessage}</ErrorMessage>
+      {label && (
+        <Label htmlFor={name} require={require}>
+          {label}
+        </Label>
+      )}
+      <S.Textarea name={name} error={error} ref={ref} size={size} {...rest} />
+      <ErrorMessage visible={!!error}>{errorMessage || error}</ErrorMessage>
     </div>
   );
 }
