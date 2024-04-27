745d39d303d122431a652e457c41cf176a686951
[SW5] 예은선 <ysvln0912@gmail.com>
2024-04-27 22:28:05 +0900

    feat : 충전소 리뷰 리스트 (전체보기) 페이지

---
+++ src/pages/chargerReviewList/ChargerReviewList.style.tsx
@@ -0,0 +1,29 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+  padding: 80px 1.5rem;
+`;
+
+export const Title = styled.h2`
+  margin-bottom: 0.5rem;
+  font-weight: ${({ theme }) => theme.FONT_WEIGHT.bold};
+`;
+
+export const Content = styled.div``;
+
+export const EmptyText = styled.div`
+  text-align: center;
+  padding-top: 100px;
+  p {
+    font-weight: ${({ theme }) => theme.FONT_WEIGHT.bold};
+    font-size: ${({ theme }) => theme.FONT_SIZE.sm};
+    color: ${({ theme }) => theme.PALETTE.gray[400]};
+    margin-bottom: 4px;
+  }
+  span {
+    display: inline-block;
+    font-weight: ${({ theme }) => theme.FONT_WEIGHT.medium};
+    font-size: ${({ theme }) => theme.FONT_SIZE.xs};
+    color: ${({ theme }) => theme.PALETTE.gray[400]};
+  }
+`;
