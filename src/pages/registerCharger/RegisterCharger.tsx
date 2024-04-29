b4a6503e3d64ea26cec2731fc5627f4e89da3aaf
JinKyu <rlawlsrb167@naver.com>
2024-04-29 20:48:42 +0900

    feat: 충전소 등록페이지input label에 require 추가

--- src/pages/registerCharger/RegisterCharger.tsx
+++ src/pages/registerCharger/RegisterCharger.tsx
@@ -113,7 +113,7 @@ export default function RegisterCharger() {
       },
       ...card,
     ]);
-    setChargerInfo((prev) => ({ ...prev, fare: "" }));
+    setChargerInfo((prev) => ({ ...prev, fare: "", speed: "급속" }));
     setChargerType(null);
   };
 
@@ -147,7 +147,8 @@ export default function RegisterCharger() {
         <S.ColumnBox>
           <S.Box>
             <SearchInput
-              label="충전소 주소 *"
+              label="충전소 주소"
+              require
               placeholder="충전소 주소를 입력해 주세요."
               error={false}
               errorMessage="필수 입력 항목입니다."
@@ -179,7 +180,9 @@ export default function RegisterCharger() {
         />
         <Label size="lg">충전기 정보</Label>
         <S.ColumnBox>
-          <Label size="md">충전 속도 *</Label>
+          <Label size="md" require>
+            충전 속도
+          </Label>
           <S.RowBox>
             <SpeedRadioBtn
               id="fast"
@@ -196,11 +199,14 @@ export default function RegisterCharger() {
           </S.RowBox>
         </S.ColumnBox>
         <S.ColumnBox>
-          <Label size="md">요금 *</Label>
+          <Label size="md" require>
+            요금
+          </Label>
           <FareInput value={chargerInfo.fare ?? ""} onChange={updateInput} />
         </S.ColumnBox>
         <SelectCharger
           label
+          require
           value={chargerType}
           onChange={updateChargerType}
           type={chargerInfo.speed === "급속" ? "fast" : "slow"}
