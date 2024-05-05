e6d22601c0786b8a5e1233668447baae35ec1173
[AI6/2반] 박재희 <dalkey23@gmail.com>
2024-05-06 04:22:13 +0900

    refactor : 상수를 활용하여 'keyword'와 placeholder를 관리함

--- src/components/pages/charger/ChargerSearch.tsx
+++ src/components/pages/charger/ChargerSearch.tsx
@@ -12,82 +12,86 @@ import LeftIcon from "@/components/common/icons/LeftIcon";
 import { ViewStyle } from "@/types";
 
 interface ChargerSearchProps {
-  searchInfo: SearchInfo;
-  searchInfoHandler: React.Dispatch<React.SetStateAction<SearchInfo>>;
-  viewtype?: ViewStyle;
+    searchInfo: SearchInfo;
+    searchInfoHandler: React.Dispatch<React.SetStateAction<SearchInfo>>;
+    viewtype?: ViewStyle;
 }
 
+const SEARCH_PLACEHOLDER = "충전소를 검색해 보세요.";
+
+const KEYWORD_NAME = "keyword";
+
 export default function ChargerSearch({
-  searchInfo,
-  searchInfoHandler,
-  viewtype = "map",
+    searchInfo,
+    searchInfoHandler,
+    viewtype = "map",
 }: ChargerSearchProps) {
-  const [show, setShow] = useState(false);
-  const [searchResults, setSearchResults] = useState<ISearchResult[]>([]);
+    const [show, setShow] = useState(false);
+    const [searchResults, setSearchResults] = useState<ISearchResult[]>([]);
 
-  const debouncedKeyword = useDebounce(searchInfo.keyword);
+    const debouncedKeyword = useDebounce(searchInfo.keyword);
 
-  useEffect(() => {
-    searchAddress(debouncedKeyword, setSearchResults);
-  }, [debouncedKeyword]);
+    useEffect(() => {
+        searchAddress(debouncedKeyword, setSearchResults);
+    }, [debouncedKeyword]);
 
-  const updateInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.currentTarget;
-    if (name === "keyword") {
-      setShow(true);
-    }
-    searchInfoHandler((prevState) => ({ ...prevState, [name]: value }));
-  };
+    const updateInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.currentTarget;
+        if (name === KEYWORD_NAME) {
+            setShow(true);
+        }
+        searchInfoHandler((prevState) => ({ ...prevState, [name]: value }));
+    };
 
-  const updateSearchItem = (name: string, location: string) => {
-    searchInfoHandler((prevState) => ({
-      ...prevState,
-      keyword: name,
-      address: {
-        ...prevState.address,
-        name,
-        location,
-      },
-    }));
-    setShow(false);
-  };
+    const updateSearchItem = (name: string, location: string) => {
+        searchInfoHandler((prevState) => ({
+            ...prevState,
+            keyword: name,
+            address: {
+                ...prevState.address,
+                name,
+                location,
+            },
+        }));
+        setShow(false);
+    };
 
-  return (
-    <S.SearchContainer viewstyle={viewtype}>
-      {viewtype === "map" ? (
-        <SearchInput
-          placeholder="충전소를 검색해보세요"
-          onChange={updateInput}
-          value={searchInfo.keyword}
-          name="keyword"
-        />
-      ) : (
-        <Input>
-          <Input.Base>
-            <Input.Left>
-              <LeftIcon />
-            </Input.Left>
-            <Input.Center
-              placeholder="충전소를 검색해 보세요."
-              onChange={updateInput}
-              value={searchInfo.keyword}
-              name="keyword"
-            />
-          </Input.Base>
-        </Input>
-      )}
+    return (
+        <S.SearchContainer viewstyle={viewtype}>
+            {viewtype === "map" ? (
+                <SearchInput
+                    placeholder={SEARCH_PLACEHOLDER}
+                    onChange={updateInput}
+                    value={searchInfo.keyword}
+                    name={KEYWORD_NAME}
+                />
+            ) : (
+                <Input>
+                    <Input.Base>
+                        <Input.Left>
+                            <LeftIcon />
+                        </Input.Left>
+                        <Input.Center
+                            placeholder={SEARCH_PLACEHOLDER}
+                            onChange={updateInput}
+                            value={searchInfo.keyword}
+                            name={KEYWORD_NAME}
+                        />
+                    </Input.Base>
+                </Input>
+            )}
 
-      {show && searchResults.length > 0 && (
-        <S.SearchResultsBox viewstyle={viewtype}>
-          {searchResults.map((result) => (
-            <SearchResultItem
-              key={result.id}
-              {...result}
-              onClick={updateSearchItem}
-            />
-          ))}
-        </S.SearchResultsBox>
-      )}
-    </S.SearchContainer>
-  );
+            {show && searchResults.length > 0 && (
+                <S.SearchResultsBox viewstyle={viewtype}>
+                    {searchResults.map((result) => (
+                        <SearchResultItem
+                            key={result.id}
+                            {...result}
+                            onClick={updateSearchItem}
+                        />
+                    ))}
+                </S.SearchResultsBox>
+            )}
+        </S.SearchContainer>
+    );
 }
