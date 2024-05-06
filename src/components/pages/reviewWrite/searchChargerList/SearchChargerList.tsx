010ec602afb4f3f3886d0a7e28e51379cdd8daad
[SW5] 예은선 <ysvln0912@gmail.com>
2024-05-06 22:20:57 +0900

    feat : searchChagerList 컴포넌트 작성

--- src/components/pages/reviewWrite/searchChargerList/SearchChargerList.tsx
+++ src/components/pages/reviewWrite/searchChargerList/SearchChargerList.tsx
@@ -1,9 +1,87 @@
+import * as S from "./SearchChargerList.style";
+
+import { useState } from "react";
+
+import { useAtom } from "jotai";
+import { reviewAtom } from "@/atoms/reviewAtom";
+
+import ChargingInfo from "@/components/common/chargingInfo/ChargingInfo";
 import SearchChargerListInput from "./input/SearchChargerListInput";
+import { ChargerStation } from "@/types/charger";
+import chargerApi from "@/apis/charger";
 
 export default function SearchChargerList() {
+  const [, setReview] = useAtom(reviewAtom);
+  const [chargerList, setChargerList] = useState<ChargerStation[]>([]);
+  const [show, setShow] = useState(false);
+  const [chargerInfo, setChargerInfo] = useState({
+    road_address_name: "",
+    address_name: "",
+    keyword: "",
+  });
+
+  const getChargerList = async () => {
+    try {
+      const response = await chargerApi.getChargerlist(
+        chargerInfo.road_address_name
+      );
+      setChargerList(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const updateSearchItem = (name: string, location: string) => {
+    setChargerInfo((info) => ({
+      ...info,
+      keyword: name,
+      address_name: name,
+      road_address_name: location,
+    }));
+    setShow(false);
+    getChargerList();
+  };
+
+  const handleGoBack = () => {
+    window.history.back();
+  };
+
+  const handleChargerClick = (chargerId: number, chargerName: string) => {
+    setReview((prev) => ({
+      ...prev,
+      chargerName: chargerName,
+      chargerId: chargerId,
+    }));
+    handleGoBack();
+  };
+
   return (
     <>
-      <SearchChargerListInput />
+      <SearchChargerListInput
+        chargerInfo={chargerInfo}
+        setChargerInfo={setChargerInfo}
+        show={show}
+        setShow={setShow}
+        onChange={updateSearchItem}
+      />
+      <S.List>
+        {chargerList?.map((chargerStation: ChargerStation) => {
+          return chargerStation.chargers.map((charger) => {
+            return (
+              <ChargingInfo
+                key={charger.chargerId}
+                info={charger}
+                like={false}
+                tag={true}
+                border="bottom"
+                onClick={() =>
+                  handleChargerClick(charger.chargerId, charger.chargerName)
+                }
+              />
+            );
+          });
+        })}
+      </S.List>
     </>
   );
 }
