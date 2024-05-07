369fa7661bc824f84cdab11bdd5e0f70cafce2db
[SW5] 예은선 <ysvln0912@gmail.com>
2024-05-08 04:39:48 +0900

    feat : 404 페이지

--- src/components/common/icons/BoltIcon.tsx
+++ src/components/common/icons/BoltIcon.tsx
@@ -1,6 +1,23 @@
-export default function BoltIcon (){
-    return <svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M2 11.5L2.5 7.65H0L4.5 0.5H5.5L5 4.9H8L3 11.5H2Z" fill="#CCCCCC"/>
+interface BoldIconProps {
+  width?: string;
+  height?: string;
+  color?: string;
+}
+
+export default function BoltIcon({
+  width = "8",
+  height = "12",
+  color = "#ccc",
+}: BoldIconProps) {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 8 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M2 11.5L2.5 7.65H0L4.5 0.5H5.5L5 4.9H8L3 11.5H2Z" fill={color} />
     </svg>
-    
+  );
 }
--- src/pages/notFound/NotFound.tsx
+++ src/pages/notFound/NotFound.tsx
@@ -1,3 +1,26 @@
+import * as S from "./NotFound.style";
+import { useNavigate } from "react-router-dom";
+import Button from "@/components/common/button/Button";
+import BoltIcon from "@/components/common/icons/BoltIcon";
+
 export default function NotFound() {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <S.Container>
+      <BoltIcon width="50" height="50" color="#FDB022" />
+      <S.TitleBox>
+        <h2>페이지를 찾을 수 없어요.</h2>
+        <p>페이지 주소를 다시 한번 확인해 주세요.</p>
+      </S.TitleBox>
+
+      <Button size="full" category="retry" onClick={handleGoBack}>
+        돌아가기
+      </Button>
+    </S.Container>
+  );
 }
