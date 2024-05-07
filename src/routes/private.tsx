1d46a31267d8f972ae5f28d493785cea5a27f8c0
[SW5] 예은선 <ysvln0912@gmail.com>
2024-05-08 03:13:42 +0900

    feat : Private 컴포넌트 작성

--- src/routes/private.tsx
+++ src/routes/private.tsx
@@ -1,4 +1,5 @@
-import { ReactNode, useEffect } from "react";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Fragment, ReactNode, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useAtom } from "jotai";
 
@@ -12,20 +13,20 @@ interface Props {
 }
 
 export default function Private({ children }: Props) {
-  const [user, setUser] = useAtom(userAtom);
+  const [user] = useAtom(userAtom);
   const { triggerToast } = useToast();
   const navigate = useNavigate();
   const location = useLocation();
   const token = TokenService.getToken();
 
   useEffect(() => {
-    // if (!token || !user.id) {
     if (!token) {
+      // if (!token || !user.id) {
       triggerToast(MESSAGE.LOGIN.REQUIRED);
-      // navigate("/login");
+      navigate("/login");
       return;
     }
   }, [location.pathname]);
 
-  return <>{children}</>;
+  return <Fragment>{children}</Fragment>;
 }
