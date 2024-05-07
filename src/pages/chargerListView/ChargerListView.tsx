c7d91561e4e085d720a9221a230f7f10aa6559fe
[AI6/2반] 박재희 <dalkey23@gmail.com>
2024-05-07 21:06:05 +0900

    feat : 충전소 목록보기에 내 근처 충전소 추가

--- src/pages/chargerListView/ChargerListView.tsx
+++ src/pages/chargerListView/ChargerListView.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import * as S from "./ChargerListView.style";
 import Button from "@/components/common/button/Button";
 import SolidMapIcon from "@/components/common/icons/SolidMapIcon";
-import { SearchInfo } from "../chargerMapView/ChargerMapView";
+import { MapCenter, SearchInfo } from "../chargerMapView/ChargerMapView";
 import ChargerSearch from "@/components/pages/charger/ChargerSearch";
 import { ChargerStation } from "@/types/charger";
 import { useToggle } from "@/hooks/useToggle";
@@ -16,7 +16,12 @@ export default function ChargerListView() {
     const navigate = useNavigate();
     const [stationId, setStationId] = useState(-1);
     const { open, close, isOpen } = useToggle(false);
-    const [filter, setFilter] = useState("");
+    const [location, setLocation] = useState("");
+    const [showRec, setShowRec] = useState(false);
+    const [mapCenter, setMapCenter] = useState<MapCenter>({
+        lat: 0,
+        lon: 0,
+    });
     const [searchInfo, setSearchInfo] = useState<SearchInfo>({
         address: {
             name: "",
@@ -34,7 +39,7 @@ export default function ChargerListView() {
 
     const [chargerInfo, setChargerInfo] = useState<ChargerStation[]>([]);
 
-    const { data, isLoading, isError } = useChargerList(filter);
+    const { data, isLoading, isError } = useChargerList(location);
 
     useEffect(() => {
         if (!isLoading && !isError) {
@@ -43,8 +48,45 @@ export default function ChargerListView() {
     }, [data, isLoading, isError]);
 
     useEffect(() => {
-        setFilter(searchInfo.address.location);
+        setShowRec(false);
+        setLocation(searchInfo.address.location);
     }, [searchInfo]);
+
+    useEffect(() => {
+        if (navigator.geolocation) {
+            // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+            navigator.geolocation.getCurrentPosition(function (position) {
+                const lat = position.coords.latitude, // 위도
+                    lon = position.coords.longitude; // 경도
+
+                setMapCenter({
+                    lat,
+                    lon,
+                });
+            });
+        } else {
+            // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+            console.log("geolocation을 사용할수 없어요..");
+        }
+    }, []);
+
+    useEffect(() => {
+        const geocoder = new window.kakao.maps.services.Geocoder();
+
+        geocoder.coord2Address(
+            mapCenter.lon,
+            mapCenter.lat,
+            async function (result: any, status: string) {
+                if (status === window.kakao.maps.services.Status.OK) {
+                    const detailAddr = !!result[0].road_address
+                        ? result[0].road_address.address_name
+                        : result[0].address.address_name;
+                    setLocation(detailAddr);
+                    setShowRec(true);
+                }
+            }
+        );
+    }, [mapCenter]);
 
     return (
         <S.ChargerContainer>
@@ -54,21 +96,49 @@ export default function ChargerListView() {
                 viewtype="list"
             />
             <S.listContainer>
-                {chargerInfo?.map((chargerStation) => {
-                    return (
-                        <div
-                            key={chargerStation.chargerGroupId}
-                            onClick={() => {
-                                setStationId(chargerStation.chargerGroupId - 1);
-                            }}>
-                            <ChargerStationSummary
-                                viewstyle="list"
-                                chargerStation={chargerStation}
-                                open={open}
-                            />
-                        </div>
-                    );
-                })}
+                {showRec ? (
+                    <div>
+                        <div>최근 검색 기록</div>
+                        <div>내 근처 충전소</div>
+                        {chargerInfo?.slice(0,5).map((chargerStation) => {
+                            return (
+                                <div
+                                    key={chargerStation.chargerGroupId}
+                                    onClick={() => {
+                                        setStationId(
+                                            chargerStation.chargerGroupId - 1
+                                        );
+                                    }}>
+                                    <ChargerStationSummary
+                                        viewstyle="list"
+                                        chargerStation={chargerStation}
+                                        open={open}
+                                    />
+                                </div>
+                            );
+                        })}
+                    </div>
+                ) : (
+                    <>
+                        {chargerInfo?.map((chargerStation) => {
+                            return (
+                                <div
+                                    key={chargerStation.chargerGroupId}
+                                    onClick={() => {
+                                        setStationId(
+                                            chargerStation.chargerGroupId - 1
+                                        );
+                                    }}>
+                                    <ChargerStationSummary
+                                        viewstyle="list"
+                                        chargerStation={chargerStation}
+                                        open={open}
+                                    />
+                                </div>
+                            );
+                        })}
+                    </>
+                )}
                 {isOpen && chargerInfo[stationId] && (
                     <ChargerListDetail
                         chargers={chargerInfo[stationId].chargers}
@@ -77,6 +147,7 @@ export default function ChargerListView() {
                     />
                 )}
             </S.listContainer>
+
             <S.ButtonContainer>
                 <Button
                     size="md"
