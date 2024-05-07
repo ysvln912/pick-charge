c906e42a002391775ea2971af61e73c211ce561c
[SW5] 예은선 <ysvln0912@gmail.com>
2024-05-08 03:12:12 +0900

    fix : 충전소 수정 및 작성 컴포넌트 수정

--- src/components/pages/reviewWrite/reviewEditContent/ReviewEditContent.tsx
+++ src/components/pages/reviewWrite/reviewEditContent/ReviewEditContent.tsx
@@ -1,82 +1,129 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import * as S from "./ReviewEditContent.style";
 
-import { Dispatch, SetStateAction, useState } from "react";
+import { useState, useEffect } from "react";
 import { useAtom } from "jotai";
 import { Link, useNavigate } from "react-router-dom";
 import { useToast } from "@/hooks/useToast";
 
+import { ReviewImage } from "@/types/review";
+import { IchargerImage } from "@/types/myCharger";
+import { reviewAtom } from "@/atoms/reviewAtom";
+import { userAtom } from "@/atoms/userAtom";
 import MESSAGE from "@/constants/message";
-import ChargerSearch from "@/components/pages/charger/ChargerSearch";
 import SearchChargerInput from "../searchChargerInput/SearchChargerInput";
 import Label from "@/components/common/label/Label";
 import PhotoRegister from "@/components/common/photoRegister/PhotoRegister";
 import StickButton from "@/components/common/stickyButton/StickyButton";
 import Textarea from "@/components/common/textarea/Textarea";
 import Rating from "@/components/pages/reviewWrite/rating/Rating";
-import { ReviewType } from "@/pages/reviewWrite/ReviewWrite";
-import { userAtom } from "@/atoms/userAtom";
-import { reviewAtom } from "@/atoms/reviewAtom";
+
+interface ReviewData {
+  content: string;
+  rating: number;
+  chargerName?: string | null;
+  chargerId?: number | null;
+}
 
 export interface ReviewEditContentProps {
-  data: ReviewType;
-  setData: Dispatch<SetStateAction<ReviewType>>;
+  submitReview: (formData: FormData) => Promise<any>;
+  submitType?: "chargerName" | "chargerId";
 }
 
 export default function ReviewEditContent({
-  data,
-  setData,
+  submitReview,
+  submitType = "chargerId",
 }: ReviewEditContentProps) {
   const [review, setReview] = useAtom(reviewAtom);
-  const [user] = useAtom(userAtom);
-
   const [error, setError] = useState({
     chargerId: "",
+    chargerName: "",
     content: "",
   });
 
-  const [chargerInfo, setChargerInfo] = useState({
-    address: {
-      name: "",
-      location: "",
-      latitude: 0,
-      longitude: 0,
-    },
-    keyword: "",
-  });
   const [photos, setPhotos] = useState<File[]>([]);
 
   const navigate = useNavigate();
   const { triggerToast } = useToast();
 
-  const { chargerId, content, rating } = review;
+  const { chargerId, content, rating, chargerName } = review;
 
   const updatePhoto = (photo: File) => {
     setPhotos((prev) => [...prev, photo]);
+
+    setReview((prev) => {
+      const updatedImgUrl = prev.imgUrl ? [...prev.imgUrl, photo] : [photo];
+      return { ...prev, imgUrl: updatedImgUrl };
+    });
   };
+
   const deletePhoto = (photos: File[]) => {
     setPhotos(photos);
+    setReview((prev) => {
+      const updatedImgUrl = photos;
+      return { ...prev, imgUrl: updatedImgUrl };
+    });
+  };
+
+  const createFormData = (data: object) => {
+    const formData = new FormData();
+    if (submitType == "chargerName") {
+      formData.append("reviewModifyPayload", JSON.stringify(data));
+    } else {
+      formData.append("reviewPayload", JSON.stringify(data));
+    }
+    photos.forEach((photo) => {
+      formData.append("imgUrl", photo);
+    });
+
+    return formData;
   };
 
   const handleUpdateValue = (value: string | number, name: string) => {
     setReview((prev) => ({ ...prev, [name]: value }));
   };
 
-  const isFormValid = chargerId && content;
+  const isFormValid = submitType && content;
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     if (!isFormValid) {
       setError({
         chargerId: chargerId ? "" : MESSAGE.REVIEW.REQUIRE,
+        chargerName: chargerName ? "" : MESSAGE.REVIEW.REQUIRE,
         content: content ? "" : MESSAGE.REVIEW.REQUIRE,
       });
       return;
     }
-    const newData = { ...review, photos, userId: user.id };
-    console.log({ newData }, "@@");
 
-    triggerToast("리뷰가 저장되었어요.", "success");
-    // navigate(`/review/${reviewId}`)
+    try {
+      const data: ReviewData = {
+        content: review.content,
+        rating: review.rating,
+      };
+
+      if (submitType == "chargerName") {
+        data.chargerName = review.chargerName;
+      } else if (submitType == "chargerId") {
+        data.chargerId = review.chargerId;
+      }
+
+      const response = await submitReview(createFormData(data));
+
+      setReview((prev) => ({
+        ...prev,
+        rating: 1,
+        chargerName: "",
+        content: "",
+        imgUrl: [],
+      }));
+      setPhotos([]);
+
+      triggerToast(MESSAGE.REVIEW.SUCCESS, "success");
+      navigate(`/review/${response}`);
+    } catch (error) {
+      triggerToast(MESSAGE.REVIEW.FAILURE, "error");
+    }
   };
 
   return (
@@ -85,9 +132,15 @@ export default function ReviewEditContent({
         <S.Box>
           <Link to="/review/write/list">
             <SearchChargerInput
-              error={error.chargerId}
+              error={
+                submitType === "chargerId"
+                  ? error.chargerId
+                  : submitType === "chargerName"
+                  ? error.chargerName
+                  : ""
+              }
               value={review.chargerName}
-              onChange={(e) => handleUpdateValue(e.target.value, "chargerId")}
+              onChange={(e) => handleUpdateValue(e.target.value, submitType)}
             />
           </Link>
         </S.Box>
@@ -107,7 +160,7 @@ export default function ReviewEditContent({
           />
         </S.Box>
         <PhotoRegister
-          photos={photos}
+          photos={review.imgUrl || []}
           updatePhoto={updatePhoto}
           deletePhoto={deletePhoto}
         />
