616710fef72e383c1d82fd5cc5944c5253adcfea
[SW5] 예은선 <ysvln0912@gmail.com>
2024-05-08 03:27:28 +0900

    fix : stickybutton write type 수정

--- src/components/common/stickyButton/StickyButton.tsx
+++ src/components/common/stickyButton/StickyButton.tsx
@@ -4,16 +4,18 @@ import * as S from "./StickyButton.style";
 export interface StickyButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement> {
   text: string;
+  position?: "default" | "write";
 }
 
 export default function StickButton({
   text,
   disabled = false,
+  position = "default",
   ...rest
 }: StickyButtonProps) {
   return (
     <>
-      <S.Container disabled={disabled} {...rest}>
+      <S.Container disabled={disabled} $position={position} {...rest}>
         <S.BtnText disabled={disabled}>{text}</S.BtnText>
       </S.Container>
     </>
