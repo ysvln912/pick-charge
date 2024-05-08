67bfed2d9e9c24a5c820bc0a6d15bd4b42ec0b49
[AI6/2반] 박재희 <dalkey23@gmail.com>
2024-05-08 15:03:55 +0900

    style : 상세페이지 스타일 수정

--- src/pages/chargerDetail/ChargerDetail.tsx
+++ src/pages/chargerDetail/ChargerDetail.tsx
@@ -16,26 +16,32 @@ import EditIcon from "@/components/common/icons/EditIcon";
 import DeleteIcon from "@/components/common/icons/DeleteIcon";
 import StickButton from "@/components/common/stickyButton/StickyButton";
 import { useChargerDetail } from "@/hooks/queries/charger";
+import PhotoSlider from "@/components/common/photoSlider/PhotoSlider";
 
 export default function ChargerDetail() {
     const navigate = useNavigate();
     const { open, close, isOpen } = useToggle(false);
     const { id } = useParams();
     const chargerId = Number(id);
-    const { data, isLoading, isError } = useChargerDetail(chargerId,1)
+    const { data, isLoading, isError } = useChargerDetail(chargerId, 1);
+    const [charger, setCharger] = useState<Charger>();
 
+    useEffect(() => {
+        const newData = { ...data, chargerRole: "개인", content : "내용이 들어가는 자리입니다. 내용이 들어가는 자리입니다. 내용이 들어가는 자리입니다. 내용이 들어가는 자리입니다. 내용이 들어가는 자리입니다. 내용이 들어가는 자리입니다. 내용이 들어가는 자리입니다." , chargerImageList : ["https://www.istockphoto.com/photo/car-rental-business-transportation-service-gm1688124066-537244903?utm_source=pixabay&utm_medium=affiliate&utm_campaign=SRP_image_sponsored&utm_content=https%3A%2F%2Fpixabay.com%2Fko%2Fimages%2Fsearch%2F%25EC%25B0%25A8%2F&utm_term=%EC%B0%A8","https://www.istockphoto.com/photo/cars-for-sale-stock-lot-row-gm1478431022-506701439?utm_source=pixabay&utm_medium=affiliate&utm_campaign=SRP_image_sponsored&utm_content=https%3A%2F%2Fpixabay.com%2Fko%2Fimages%2Fsearch%2F%25EC%25B0%25A8%2F&utm_term=%EC%B0%A8"],myChargerCheck : false};
+        if (!isLoading && !isError) {
+            setCharger(newData);
+        }
+    }, [data, isLoading, isError]);
 
     function handleDelete() {
         console.log(`충전소 삭제 api요청`);
     }
 
     function handleEmptyLike() {
-       
         console.log(`즐겨찾기 추가 api요청`);
     }
 
     function handleLike() {
-        
         console.log(`즐겨찾기 삭제 api요청`);
     }
 
@@ -49,26 +55,29 @@ export default function ChargerDetail() {
     function LikeButton() {
         return <IconButton icon="like" onClick={handleLike} />;
     }
-
-    if (!data) {
-        return <></>;
-    }
+    console.log(charger)
 
     return (
         <S.ChargerContainer>
             <TopNavigationBar
                 leftBtn={<IconButton icon="arrowDown" />}
-                text={data.chargerName}
-                rightBtn={data.favorite ? <LikeButton /> : <EmptyLikeButton />}
+                text={charger?.chargerName}
+                rightBtn={
+                    (
+                        charger?.myChargerCheck && <MoreIconButton />
+                    ) || (
+                        charger?.favorite ? <LikeButton /> : <EmptyLikeButton />
+                    )
+                }
             />
             <S.ChargerOverview>
                 <S.ChargerCompany>회사명</S.ChargerCompany>
                 <S.ChargerStatus>
-                    <ChargingRoleCard role={data.chargerRole} />
-                    <RatingWithStar rating={data.avgRate} />
+                    <ChargingRoleCard role={charger?.chargerRole || ""} />
+                    <RatingWithStar rating={charger?.avgRate} />
                 </S.ChargerStatus>
-                <S.Title>{data.chargerName}</S.Title>
-                <S.ChargerAddress>{data.chargerLocation}</S.ChargerAddress>
+                <S.Title>{charger?.chargerName}</S.Title>
+                <S.ChargerAddress>{charger?.chargerLocation}</S.ChargerAddress>
             </S.ChargerOverview>
             <S.ChargerInfo>
                 <S.Title>충전기 정보</S.Title>
@@ -82,16 +91,18 @@ export default function ChargerDetail() {
                     <tbody>
                         <tr>
                             <td>
-                                <ChargerStatus status={data.chargerStatus} />
+                                <ChargerStatus
+                                    status={charger?.chargerStatus || ""}
+                                />
                             </td>
                             <td>
-                                {/* {data.chargerTypeList.map((chargerType) => {
+                                {charger?.chargerTypeList.map((chargerType) => {
                                     return (
                                         <span key={chargerType.id}>
                                             {chargerType.type}
                                         </span>
                                     );
-                                })} */}
+                                })}
                             </td>
                         </tr>
                     </tbody>
@@ -99,39 +110,68 @@ export default function ChargerDetail() {
             </S.ChargerInfo>
             <S.ChargerPrice>
                 <S.Title>충전 요금</S.Title>
-                <p>{data.chargingSpeed}</p>
                 <S.PriceInfo>
-                    <div className="price-quantity  row">100kWh</div>
+                    <div className="charging-speed row">
+                        {charger?.chargingSpeed}
+                    </div>
                     <div className="price-member row">
-                        <div>회원가</div>
-                        <div>비회원가</div>
+                        {charger?.chargerRole === "공공" ? (
+                            <>
+                                <div>회원가</div>
+                                <div>비회원가</div>
+                            </>
+                        ) : (
+                            <div></div>
+                        )}
                     </div>
                     <div className="price-rate row">
-                        <div>
-                            <span>{data.memberPrice}원</span> /kWh
-                        </div>
-                        <div>
-                            <span>{data.nonmemberPrice}원</span> /kWh
-                        </div>
+                        {charger?.chargerRole === "공공" ? (
+                            <>
+                                <div>
+                                    <span>{charger?.memberPrice}원</span> /kWh
+                                </div>
+                                <div>
+                                    <span>{charger?.nonmemberPrice}원</span>{" "}
+                                    /kWh
+                                </div>
+                            </>
+                        ) : (
+                            <div>
+                                <span>{charger?.personalPrice}원</span> /kWh
+                            </div>
+                        )}
                     </div>
                 </S.PriceInfo>
             </S.ChargerPrice>
+            <S.StationInfo>
+            {charger?.chargerRole === "공공" ? (
+                <></>
+            ) : (
+                <div>
+                    <S.Title>충전소 정보</S.Title>
+                    <S.StationContent>
+                    <PhotoSlider imgs={charger?.chargerImageList || []} category="charging"/>
+                    <p>{charger?.content}</p>
+                    </S.StationContent>
+                </div>
+            )}
+            </S.StationInfo>
             <S.ChargerReview>
                 <div className="reviewTitle">
                     <S.Title>충전소 리뷰 </S.Title>
-                    <Link to={`/charger/${data.chargerId}/reviews`}>
+                    <Link to={`/charger/${charger?.chargerId}/reviews`}>
                         전체보기
                         <ArrowRightIcon />
                     </Link>
                 </div>
 
-                {/* {data.reviewList && data.reviewList.length !== 0 ? (
-                    data.reviewList.map((review) => {
+                {charger?.reviewList && charger?.reviewList.length !== 0 ? (
+                    charger?.reviewList.map((review) => {
                         return (
                             <ReviewItem
                                 key={review.id}
                                 date={review.modified_at}
-                                address={data.chargerName}
+                                address={charger?.chargerName}
                                 rating={String(review.rating)}
                                 review={review.content}
                                 onClick={() => {
@@ -145,9 +185,9 @@ export default function ChargerDetail() {
                         <S.SubTitle>아직 리뷰가 없어요!</S.SubTitle>
                         <p>충전소에 대한 경험을 공유해 주세요</p>
                     </S.EmptyReview>
-                )} */}
+                )}
             </S.ChargerReview>
-            {data.chargerRole === "개인" ? (
+            {charger?.chargerRole === "개인" ? (
                 <StickButton
                     text="문의하기"
                     onClick={() => {
