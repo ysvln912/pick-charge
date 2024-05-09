4c7e8c7532d33ab42e09bbeb854a0a893d6a15cf
[SW5] 예은선 <ysvln0912@gmail.com>
2024-05-10 02:38:15 +0900

    fix : logout cookie 만료

--- src/utils/logout.ts
+++ src/utils/logout.ts
@@ -1,6 +1,8 @@
 import TokenService from "./tokenService";
 
 const logout = () => {
+  document.cookie =
+    "refreshToken=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
   TokenService.removeToken();
 };
 
