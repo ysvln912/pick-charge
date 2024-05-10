abe6149024dd2ce54b4b6f76b6e3b0a6117e4cfd
[AI6/2반] 박재희 <dalkey23@gmail.com>
2024-05-11 03:46:51 +0900

    style : 목록 검색 시 아이콘 수정

--- src/components/pages/charger/ChargerSearch.style.tsx
+++ src/components/pages/charger/ChargerSearch.style.tsx
@@ -3,43 +3,46 @@ import { flexColumn } from "@/styles/common";
 import { ViewStyle } from "@/types";
 
 const inputview = {
-  map: css`
-    position: absolute;
-    padding: 1.5rem;
-    width: 100%;
-    z-index: 2;
-  `,
-  list: css`
-    position: fixed;
-    top: 0;
-    width: 390px;
-    div div {
-      border-radius: 0;
-    }
-  `,
+    map: css`
+        position: absolute;
+        padding: 1.5rem;
+        width: 100%;
+        z-index: 2;
+    `,
+    list: css`
+        position: fixed;
+        top: 0;
+        width: 390px;
+        div div {
+            border-radius: 0;
+        }
+        path {
+            fill: ${({ theme }) => theme.PALETTE.gray[200]};
+        }
+    `,
 };
 
 const resultview = {
-  map: css`
-    border-radius: 5px;
-    border: 1px solid ${({ theme }) => theme.PALETTE.gray[200]};
-    background-color: ${({ theme }) => theme.PALETTE.white};
-    ${flexColumn};
-    position: absolute;
-    width: 87.5%;
-    top: 70px;
-    z-index: 2;
-  `,
-  list: css`
-    border: 1px solid ${({ theme }) => theme.PALETTE.gray[200]};
-    background-color: ${({ theme }) => theme.PALETTE.white};
-  `,
+    map: css`
+        border-radius: 5px;
+        border: 1px solid ${({ theme }) => theme.PALETTE.gray[200]};
+        background-color: ${({ theme }) => theme.PALETTE.white};
+        ${flexColumn};
+        position: absolute;
+        width: 87.5%;
+        top: 70px;
+        z-index: 2;
+    `,
+    list: css`
+        border: 1px solid ${({ theme }) => theme.PALETTE.gray[200]};
+        background-color: ${({ theme }) => theme.PALETTE.white};
+    `,
 };
 
 export const SearchContainer = styled.div<{ viewstyle: ViewStyle }>`
-  ${({ viewstyle }) => inputview[viewstyle]}
+    ${({ viewstyle }) => inputview[viewstyle]}
 `;
 
 export const SearchResultsBox = styled.div<{ viewstyle: ViewStyle }>`
-  ${({ viewstyle }) => resultview[viewstyle]}
+    ${({ viewstyle }) => resultview[viewstyle]}
 `;
